69ffb12ec1809e5d4308f7819768c0e06c5cda7a
taylorkraft <taylorkraft16@gmail.com>
2020-03-22 13:40:32 -0600

    App Controller routes added/ Mountain controller routes added

--- app/controllers/mountain_controller.rb
+++ app/controllers/mountain_controller.rb
@@ -1,4 +1,9 @@
 class MountainController < ApplicationController 
 
-
+  get '/mountains' do
+    @mountains = Mountain.all
+    
+    erb :"mountains/index"
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  validates_presence_of :name, :email, :password
   has_many :mountains
 end
